a5dfbe491b6e0cd8cf4e64f17f66772a7ebb96c6
Saller12 <saller.josef@johak.at>
2022-11-29 11:31:55 +0100

    student

---
+++ src/components/Studentlist.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref} from "vue";
+import { onMounted } from 'vue';
+const studentlist = ref(null);
+onMounted(async () => {
+const response = await fetch("https://pb.seiwald.club/api/collections/4bhk/records")
+const data = await response.json();
+});
+</script>
+
+<template>
+    <div>
+        <h1>Our Students</h1>
+        <ul>
+        <li v-for="student in students" :key="student.id">
+            {{ student.name }}
+        </li>
+        </ul>
+    </div>
+</template>
