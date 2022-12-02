228d1bd94f7dd6a69d5b7e975d6a8b26e6d344e5
Saller12 <saller.josef@johak.at>
2022-12-02 13:07:15 +0100

    delete and push

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <script setup>
 // Import your components here:
 import HelloJOHAK from "./components/HelloJOHAK.vue";
+import Studentlist from "./components/Studentlist.vue";
 </script>
 
 <template>
   <!-- Use your components here: -->
   <HelloJOHAK></HelloJOHAK>
+  <Studentlist></Studentlist>
   <div>
-    <p>
-      built with <img src="/vite.svg" alt="vite-logo" /> &
-      <img src="/vue.svg" alt="" />
-    </p>
   </div>
 </template>
--- src/components/Studentlist.vue
+++ src/components/Studentlist.vue
@@ -1,20 +1,60 @@
 <script setup>
 import { ref} from "vue";
 import { onMounted } from 'vue';
-const studentlist = ref(null);
+const baseUrl= "https://pb.seiwald.club/api/collections/4bhk/records"
+const studentlist = ref([]);
 onMounted(async () => {
-const response = await fetch("https://pb.seiwald.club/api/collections/4bhk/records")
-const data = await response.json();
+    getStudents();
 });
+let schuelerVorname= ref("");
+let schuelerNachname= ref("");
+let schuelerwohnort= ref("");
+let schuelergeburtstag= ref(null);
+async function addStudent() {
+    const newStudent = {
+            vorname: schuelerVorname.value,
+            nachname: schuelerNachname.value,
+            wohnort: schuelerwohnort.value,
+            geburtstag: schuelergeburtstag.value,
+        }
+    const response = await fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newStudent)
+    });
+}
+async function getStudents(){
+    const response = await fetch(baseUrl)
+const data = await response.json();
+studentlist.value = data.items;
+
+}
+
+async function deleteStudent(id){
+    await fetch(`${baseUrl}/${id}`, {
+        method: "DELETE",
+    });
+    getStudents();
+}
 </script>
 
 <template>
-    <div>
+    <div>     
         <h1>Our Students</h1>
         <ul>
-        <li v-for="student in students" :key="student.id">
-            {{ student.name }}
+        <li v-for="student in studentlist" :key="student.id">
+            {{ student.vorname }}, {{ student.nachname }} wohnt in {{ student.wohnort }} und ist am {{ student.geburtstag.substring(0,10) }} geboren.
+            <button @click="deleteStudent(student.id)">X</button>
         </li>
         </ul>
     </div>
+    <form>
+    <input type="text" v-model="schuelerVorname" placeholder="Vorname"/>
+    <input type="text" v-model="schuelerNachname" placeholder="Nachname"/>
+    <input type="text" v-model="schuelerwohnort" placeholder="Wohnort"/>
+    <input type="date" v-model="schuelergeburtstag" placeholder="Geburtsdatum"/>
+    <button @click.prevent="addStudent">Add Student</button>
+</form>
 </template>
